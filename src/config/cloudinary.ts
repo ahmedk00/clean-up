4a79f4d6d2a7f7bfc43ba21e615b649e48992bf7
yomna khaled <yomnakhaled@yomnas-MacBook-Air.local>
2025-11-19 03:19:34 +0200

    fix multer bug

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,23 +1,22 @@
-import cloudinary from "cloudinary";
-import CloudinaryStorage from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { env } from "./env";
 
 // Configure Cloudinary
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET,
 });
 
-// Create storage for previous work images
-export const previousWorkStorage = CloudinaryStorage({
-  cloudinary: cloudinary.v2,
+// Create Cloudinary storage instance
+export const previousWorkStorage = new CloudinaryStorage({
+  cloudinary,
   params: {
     folder: "cleaning-services/previous-work",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
     transformation: [{ width: 1200, height: 800, crop: "limit" }],
-  } as any,
+  },
 });
 
 export { cloudinary };
-export const cloudinaryV2 = cloudinary.v2;
